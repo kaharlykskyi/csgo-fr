55c55e48d77dfb83bd52f5907d3cb7801ef8ce11
S_Vasyl <selivonenko007@gmail.com>
2018-08-12 18:35:29 +0300

    upcoming matches

--- resources/views/home/components/latest_matches.blade.php
+++ resources/views/home/components/latest_matches.blade.php
@@ -1,5 +1,5 @@
 <div class="nk-gap-2"></div>
-<h3 class="nk-decorated-h-2"><span><span class="text-main-1">Live</span> Matche</span></h3>
+<h3 class="nk-decorated-h-2"><span><span class="text-main-1">Live</span> Matches</span></h3>
 <div class="nk-gap"></div>
 <div class="row">
     <div class="col-12">
@@ -39,7 +39,82 @@
         <a href="tournaments.html" class="nk-btn nk-btn-rounded nk-btn-color-main-1">Match Details</a>
     </div>
 </div>
-<div class="nk-gap"></div>
+
+<div class="nk-gap-4"></div>
+
+<h3 class="nk-decorated-h-2"><span><span class="text-main-1">UPCOMING </span> MATCHES</span></h3>
+
+<div class="row">
+    <div class="col-12">
+        <div class="nk-gap"></div>
+        <div class="nk-match">
+            <div class="nk-match-team-left">
+                <a href="#">
+                        <span class="nk-match-team-logo">
+                            <img src="{{ asset('images/team-1.jpg') }}" alt="">
+                        </span>
+                    <span class="nk-match-team-name">
+                            SK Telecom T1
+                        </span>
+                </a>
+            </div>
+            <div class="nk-match-status">
+                <a href="#">
+                    <span class="nk-match-status-vs">VS</span>
+                    <span class="nk-match-status-date">Apr 28, 2018 8:00 pm</span>
+                </a>
+            </div>
+            <div class="nk-match-team-right">
+                <a href="#">
+                        <span class="nk-match-team-name">
+                            Cloud 9
+                        </span>
+                    <span class="nk-match-team-logo">
+                            <img src="{{ asset('images/team-2.jpg') }}" alt="">
+                        </span>
+                </a>
+            </div>
+        </div>
+        <a href="tournaments.html" class="nk-btn nk-btn-rounded nk-btn-color-main-1 mt-10">Match Details</a>
+    </div>
+    <div class="nk-gap"></div>
+    <div class="col-12">
+        <div class="nk-gap"></div>
+        <div class="nk-match">
+            <div class="nk-match-team-left">
+                <a href="#">
+                        <span class="nk-match-team-logo">
+                            <img src="{{ asset('images/team-1.jpg') }}" alt="">
+                        </span>
+                    <span class="nk-match-team-name">
+                            SK Telecom T1
+                        </span>
+                </a>
+            </div>
+            <div class="nk-match-status">
+                <a href="#">
+                    <span class="nk-match-status-vs">VS</span>
+                    <span class="nk-match-status-date">Apr 28, 2018 8:00 pm</span>
+                </a>
+            </div>
+            <div class="nk-match-team-right">
+                <a href="#">
+                        <span class="nk-match-team-name">
+                            Cloud 9
+                        </span>
+                    <span class="nk-match-team-logo">
+                            <img src="{{ asset('images/team-2.jpg') }}" alt="">
+                        </span>
+                </a>
+            </div>
+        </div>
+        <a href="tournaments.html" class="nk-btn nk-btn-rounded nk-btn-color-main-1 mt-10">Match Details</a>
+    </div>
+    <div class="nk-gap"></div>
+</div>
+
+<div class="nk-gap-4"></div>
+
 <h3 class="nk-decorated-h-2"><span><span class="text-main-1">Latest</span> Matches</span></h3>
 <div class="nk-gap"></div>
 <div class="nk-match">
