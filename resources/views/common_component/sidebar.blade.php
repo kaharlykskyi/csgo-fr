12863c732dfe6cc3f1e5a4cdad9e978d86ee1022
S_Vasyl <selivonenko007@gmail.com>
2018-08-12 18:06:03 +0300

    edits for home page

--- resources/views/common_component/sidebar.blade.php
+++ resources/views/common_component/sidebar.blade.php
@@ -9,204 +9,33 @@
             </form>
         </div>
     </div>
-    <div class="nk-widget nk-widget-highlighted">
-        <h4 class="nk-widget-title"><span><span class="text-main-1">Live</span> Matches</span></h4>
-        <div class="nk-widget-content">
-            <div class="nk-widget-match">
-                <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <figure class="figure">
-                               <img src="{{ asset('images/team-1.jpg') }}" alt="">
-                              <figcaption class="figure-caption">team1</figcaption>
-                            </figure>
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <figure class="figure">
-                               <img src="{{ asset('images/team-2.jpg') }}" alt="">
-                              <figcaption class="figure-caption">team2</figcaption>
-                            </figure>
-                        </span>
-                    </span>
-                </span>
-                    <span class="nk-widget-match-right">
-                        <span class="nk-match-score bg-dark-1">
-                            0 : 0
-                        </span>
-                    </span>
-                    <a href="#" class="btn-side-bar nk-btn nk-btn-rounded nk-btn-color-main-1">Match Details</a>
-                </a>
-            </div>
-
-            <div class="nk-widget-match">
-                <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <figure class="figure">
-                               <img src="{{ asset('images/team-1.jpg') }}" alt="">
-                              <figcaption class="figure-caption">team1</figcaption>
-                            </figure>
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <figure class="figure">
-                               <img src="{{ asset('images/team-2.jpg') }}" alt="">
-                              <figcaption class="figure-caption">team2</figcaption>
-                            </figure>
-                        </span>
-                    </span>
-                    </span>
-                        <span class="nk-widget-match-right">
-                        <span class="nk-match-score bg-dark-1">
-                            0 : 0
-                        </span>
-                    </span>
-                    <a href="#" class="btn-side-bar nk-btn nk-btn-rounded nk-btn-color-main-1">Match Details</a>
-                </a>
-            </div>
-        </div>
-    </div>
 
     <div class="nk-widget nk-widget-highlighted">
-        <h4 class="nk-widget-title"><span><span class="text-main-1">Next</span> Matches</span></h4>
+        <h4 class="nk-widget-title"><span><span class="text-main-1">LIVE </span> STREAMS</span></h4>
         <div class="nk-widget-content">
-            <div class="nk-widget-match">
+            <div class="nk-widget-match p-5">
                 <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-1.jpg') }}" alt="">
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-2.jpg') }}" alt="">
-                        </span>
-                    </span>
-                    <span class="nk-widget-match-date">CS:GO - Apr 28, 2018 8:00 pm</span>
-                </span>
-                    <span class="nk-widget-match-right">
-                    <span class="nk-match-score">
-                        Upcoming
-                    </span>
-                </span>
+                    <div class="nk-widget-stream">
+                        <span class="nk-widget-stream-status bg-success"></span>
+                        <div class="nk-widget-stream-flag">
+                            <img src="{{asset('images/flag/brunei.png')}}" alt="">
+                        </div>
+                        <div class="nk-widget-stream-name">ESL TV</div>
+                        <span class="nk-widget-stream-count">1234 viewers</span>
+                    </div>
                 </a>
             </div>
 
-            <div class="nk-widget-match">
+            <div class="nk-widget-match p-5">
                 <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-3.jpg') }}" alt="">
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-2.jpg') }}" alt="">
-                        </span>
-                    </span>
-                    <span class="nk-widget-match-date">LoL - Apr 24, 2018 7:20 pm</span>
-                </span>
-                    <span class="nk-widget-match-right">
-                    <span class="nk-match-score">
-                        Upcoming
-                    </span>
-                </span>
-                </a>
-            </div>
-
-            <div class="nk-widget-match">
-                <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-1.jpg') }}" alt="">
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-4.jpg') }}" alt="">
-                        </span>
-                    </span>
-                    <span class="nk-widget-match-date">Dota 2 - Apr 12, 2018 6:40 pm</span>
-                </span>
-                    <span class="nk-widget-match-right">
-                    <span class="nk-match-score bg-dark-1">
-                        0 : 0
-                    </span>
-                </span>
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="nk-widget nk-widget-highlighted">
-        <h4 class="nk-widget-title"><span><span class="text-main-1">Latest</span> Matches</span></h4>
-        <div class="nk-widget-content">
-            <div class="nk-widget-match">
-                <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-1.jpg') }}" alt="">
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-2.jpg') }}" alt="">
-                        </span>
-                    </span>
-                    <span class="nk-widget-match-date">CS:GO - Apr 28, 2018 8:00 pm</span>
-                </span>
-                    <span class="nk-widget-match-right">
-                    <span class="nk-match-score bg-dark-1">
-                        0 : 0
-                    </span>
-                </span>
-                </a>
-            </div>
-
-            <div class="nk-widget-match">
-                <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-3.jpg') }}" alt="">
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-2.jpg') }}" alt="">
-                        </span>
-                    </span>
-                    <span class="nk-widget-match-date">LoL - Apr 24, 2018 7:20 pm</span>
-                </span>
-                    <span class="nk-widget-match-right">
-                    <span class="nk-match-score bg-dark-1">
-                        0 : 0
-                    </span>
-                </span>
-                </a>
-            </div>
-
-            <div class="nk-widget-match">
-                <a href="#">
-                <span class="nk-widget-match-left">
-                    <span class="nk-widget-match-teams">
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-1.jpg') }}" alt="">
-                        </span>
-                        <span class="nk-widget-match-vs">VS</span>
-                        <span class="nk-widget-match-team-logo">
-                            <img src="{{ asset('images/team-4.jpg') }}" alt="">
-                        </span>
-                    </span>
-                    <span class="nk-widget-match-date">Dota 2 - Apr 12, 2018 6:40 pm</span>
-                </span>
-                    <span class="nk-widget-match-right">
-                    <span class="nk-match-score bg-dark-1">
-                        0 : 0
-                    </span>
-                </span>
+                    <div class="nk-widget-stream">
+                        <span class="nk-widget-stream-status bg-danger"></span>
+                        <div class="nk-widget-stream-flag">
+                            <img src="{{asset('images/flag/brazil.png')}}" alt="">
+                        </div>
+                        <div class="nk-widget-stream-name">FcS</div>
+                        <span class="nk-widget-stream-count"></span>
+                    </div>
                 </a>
             </div>
         </div>
