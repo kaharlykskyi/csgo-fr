fc4a4faa5d74b1c7ded6ec661585a6d08b0d0512
S_Vasyl <selivonenko007@gmail.com>
2018-09-19 15:29:55 +0300

    topic description fix

--- resources/views/forum/topic_page.blade.php
+++ resources/views/forum/topic_page.blade.php
@@ -64,7 +64,7 @@
                         <div class="nk-forum-title">
                             <h3><a href="{{route('thread_page',['id' => $topic->id, 'thread_id' => $thread->id])}}">{{$thread->title}}</a></h3>
                             @isset($thread->description)
-                                <div class="nk-forum-title-sub">{{ str_limit($thread->description, 50, ' ...')  }}</div>
+                                <div class="nk-forum-title-sub">{{ str_limit(str_limit(strip_tags($thread->description), 50, ' ...'), 50, ' ...')  }}</div>
                             @endisset
                             <div class="nk-forum-title-sub">Started by <a href="#">
                                     @isset($users)
@@ -135,7 +135,7 @@
                         <div class="nk-forum-title">
                             <h3><a href="@if($thread->state == 1){{route('thread_page',['id' => $topic->id, 'thread_id' => $thread->id])}}@endif">{{$thread->title}}</a></h3>
                             @isset($thread->description)
-                                <div class="nk-forum-title-sub">{!! $thread->description !!}</div>
+                                <div class="nk-forum-title-sub">{{ str_limit(strip_tags($thread->description), 50, ' ...')  }}</div>
                             @endisset
                             <div class="nk-forum-title-sub">Started by
                                 <a href="#">
