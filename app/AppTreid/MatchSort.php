1edf6429738302a1e40fa5661195645aade1704c
S_Vasyl <selivonenko007@gmail.com>
2018-10-08 15:41:59 +0300

    minor fix(match scoreboard)

--- app/AppTreid/MatchSort.php
+++ app/AppTreid/MatchSort.php
@@ -22,7 +22,7 @@ trait MatchSort
         $count_live_match_scoreboard = DB::table('settings')->where('name','=','count_live_match_scoreboard')->select('value')->first();
         $count_upcoming_match_scoreboard = DB::table('settings')->where('name','=','count_upcoming_match_scoreboard')->select('value')->first();
 
-        $latest_match = Match::whereRaw("TIMESTAMPDIFF(MINUTE, match_day, NOW()) > 120")->limit((int)$count_latest_match_scoreboard->value)->get();
+        $latest_match = Match::whereRaw("TIMESTAMPDIFF(MINUTE, match_day, NOW()) > 60")->limit((int)$count_latest_match_scoreboard->value)->get();
         $live_match = Match::whereRaw("TIMESTAMPDIFF(MINUTE, NOW(), match_day) BETWEEN -120 AND 60")->limit((int)$count_live_match_scoreboard->value)->get();
         $upcoming_matches = Match::whereRaw("TIMESTAMPDIFF(MINUTE, NOW(), match_day) > 120")->limit((int)$count_upcoming_match_scoreboard->value)->get();
 
