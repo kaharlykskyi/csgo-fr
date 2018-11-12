a16ce618ba119dccb9034335c6960d53db3ce523
S_Vasyl <selivonenko007@gmail.com>
2018-11-12 10:50:10 +0200

    login cookies

--- resources/views/common_component/login_modal.blade.php
+++ resources/views/common_component/login_modal.blade.php
@@ -31,6 +31,15 @@
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
+
+                                <div class="nk-gap"></div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
                             </div>
                         </div>
 
